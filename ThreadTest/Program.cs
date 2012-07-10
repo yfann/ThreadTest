63bb919fccf714b1401b0cb0db86a96b97c608bc
Fan Yu <yufan@dev-yufan.visual-2000.cn>
2012-07-10 17:25:03 +0800

    async task

--- ThreadTest/Program.cs
+++ ThreadTest/Program.cs
@@ -3,11 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading;
+using System.Threading.Tasks;
 
 namespace ThreadTest
 {
     internal class Program
     {
+        public delegate void DoSomethind();
+
         private static void Main(string[] args)
         {
             //Thread[] threads = new Thread[10];
@@ -63,17 +66,28 @@ namespace ThreadTest
             //t2.Start();
 
             //mutex test
-            MutexTest mu = new MutexTest();
-            Thread[] ts = new Thread[10];
-            for (int i = 0; i < 10; i++)
-            {
-                ts[i] = new Thread(mu.Run);
-            }
-            for (int i = 0; i < 10; i++)
-            {
-                ts[i].Start();
-            }
+            //MutexTest mu = new MutexTest();
+            //Thread[] ts = new Thread[10];
+            //for (int i = 0; i < 10; i++)
+            //{
+            //    ts[i] = new Thread(mu.Run);
+            //}
+            //for (int i = 0; i < 10; i++)
+            //{
+            //    ts[i].Start();
+            //}
 
+            //async
+            //DoSomethind dosth = new DoSomethind(() => { Console.WriteLine("do some thing"); });
+            //IAsyncResult result = dosth.BeginInvoke(new AsyncCallback(async =>
+            //{
+            //    Console.WriteLine("before end invoke");
+            //    dosth.EndInvoke(async);
+            //    Console.WriteLine(async.AsyncState.ToString());
+            //}), "test");
+            //Console.WriteLine("end");
+
+            TaskTest();
             Console.ReadLine();
         }
 
@@ -85,6 +99,43 @@ namespace ThreadTest
         public static void ThreadTest2(object str)
         {
             Console.WriteLine("{0} {1}", Thread.CurrentThread.ManagedThreadId, str.ToString());
+        }
+
+        public static void TaskTest()
+        {
+            Action<object> action = (object obj) =>
+            {
+                Console.WriteLine("Task={0}, obj={1}, Thread={2}", Task.CurrentId, obj.ToString(), Thread.CurrentThread.ManagedThreadId);
+            };
+
+            // Construct an unstarted task
+            Task t1 = new Task(action, "alpha");
+
+            // Cosntruct a started task
+            Task t2 = Task.Factory.StartNew(action, "beta");
+
+            // Block the main thread to demonstate that t2 is executing
+            t2.Wait();
+
+            // Launch t1
+            t1.Start();
+
+            Console.WriteLine("t1 has been launched. (Main Thread={0})", Thread.CurrentThread.ManagedThreadId);
+
+            // Wait for the task to finish.
+            // You may optionally provide a timeout interval or a cancellation token
+            // to mitigate situations when the task takes too long to finish.
+            t1.Wait();
+
+            // Construct an unstarted task
+            Task t3 = new Task(action, "gamma");
+
+            // Run it synchronously
+            t3.RunSynchronously();
+
+            // Although the task was run synchrounously, it is a good practice to wait for it which observes for
+            // exceptions potentially thrown by that task.
+            t3.Wait();
         }
     }
 
